14488457383ceb21d659adfddec8a1147fbc945c
miyaluc <srlucas3@gmail.com>
2017-10-04 10:24:38 -0700

    created tables and databases

---
+++ lab17miya/lab17miya/Data/lab17miyaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab17miya.Models
+{
+    public class lab17miyaContext : DbContext
+    {
+        public lab17miyaContext (DbContextOptions<lab17miyaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab17miya.Models.Destination> Destination { get; set; }
+    }
+}
---
+++ lab17miya/lab17miya/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lab17miya.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
