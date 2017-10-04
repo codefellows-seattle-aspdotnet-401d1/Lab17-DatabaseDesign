de3ed6f0667ca5ec52bab3abd8d2c1b2abad01ec
Cameron Pam <cammpam.creative@gmail.com>
2017-10-04 11:26:09 -0700

    Models Made

---
+++ Lab17Cameron/Lab17Cameron/Models/Destination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab17Cameron.Models
+{
+    public class Destination
+    {
+        [Key]
+        public int DestinationID { get; set; }
+        public int SeasonID { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ Lab17Cameron/Lab17Cameron/Models/Season.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab17Cameron.Models
+{
+    public class Season
+    {
+        [Key]
+        public int SeasonID { get; set; }
+        public int DestinationID { get; set; }
+        public int Winter { get; set; }
+        public int Spring { get; set; }
+        public int Summer { get; set; } 
+        public int Fall { get; set; }
+    }
+}
