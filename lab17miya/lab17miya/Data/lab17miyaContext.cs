f0ba340e9331a312bca5c621f4d5c56df25ba61b
miyaluc <srlucas3@gmail.com>
2017-10-04 13:33:43 -0700

    created additional database(s)

--- lab17miya/lab17miya/Data/lab17miyaContext.cs
+++ lab17miya/lab17miya/Data/lab17miyaContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using lab17miya.Models;
 
 namespace lab17miya.Models
 {
@@ -14,5 +15,9 @@ namespace lab17miya.Models
         }
 
         public DbSet<lab17miya.Models.Destination> Destination { get; set; }
+
+        public DbSet<lab17miya.Models.Supplies> Supplies { get; set; }
+
+        public DbSet<lab17miya.Models.Weather> Weather { get; set; }
     }
 }
--- lab17miya/lab17miya/Migrations/20171004185253_Quaternary.Designer.cs
+++ lab17miya/lab17miya/Migrations/20171004185253_Quaternary.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace lab17miya.Migrations
 {
     [DbContext(typeof(lab17miyaContext))]
-    partial class lab17miyaContextModelSnapshot : ModelSnapshot
+    [Migration("20171004185253_Quaternary")]
+    partial class Quaternary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
