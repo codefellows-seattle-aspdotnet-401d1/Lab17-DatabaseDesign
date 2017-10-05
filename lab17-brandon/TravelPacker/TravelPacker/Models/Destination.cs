2ae4eec48145fe4d15ff4f11ac811a5810b343fd
Brandon Little <brlittle86@gmail.com>
2017-10-04 23:19:19 -0700

    created Destination model

---
+++ lab17-brandon/TravelPacker/TravelPacker/Models/Destination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelPacker.Models
+{
+    public class Destination
+    {
+        public int ID { get; set; }
+        public string LocationName { get; set; }
+        public string Country { get; set; }
+        public string Climate { get; set; }
+        public ICollection<int> SuppliesIDs { get; set; }
+
+        public Destination(string locName, string country, string climate, ICollection<int> supplies)
+        {
+            ICollection<int> SuppliesIDs = new List<int>();
+            LocationName = locName;
+            Country = country;
+            Climate = climate;
+            SuppliesIDs = supplies;
+        }
+    }
+}
